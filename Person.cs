eb59aed06417c968a13a9cf8b4c5eabe3b7869ab
Hannah Benkert <theo.albert.blakey@outlook.com>
2023-06-18 17:33:50 +0100

    first commit

---
+++ Person.cs
@@ -0,0 +1,10 @@
+class Person
+{
+    public string Name  // property
+    { get; set; }
+
+    public Person(string Name)
+    {
+        this.Name = Name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Bob!");
+
+Person person = new Person("Jim");
+person.Name = "this has been added to";
+
+Console.WriteLine(person.Name);
